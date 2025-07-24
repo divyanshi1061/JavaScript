1831e6f75bf7407c1da7d5d46728cf2e7922c510
Divyanshi <109888817+divyanshi1061@users.noreply.github.com>
2025-07-24 05:06:25 +0000

    datatypes

---
+++ 1_basics/practice.js
@@ -0,0 +1,12 @@
+// Variables not use var
+const accountId=144553
+let accountEmail="divyanshikhandelwal755@gmail.com"
+var accountPassword= "12345"
+accountCity="Jaipur"
+
+// accountId=2 not allowed
+console.log(accountId);
+
+
+// data types
+
