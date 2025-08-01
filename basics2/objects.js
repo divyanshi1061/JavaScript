3b5657a191c3cea1d000361e21b5494ca908ae8e
Divyanshi <109888817+divyanshi1061@users.noreply.github.com>
2025-08-01 06:43:11 +0000

    container config

--- basics2/objects.js
+++ basics2/objects.js
@@ -1,3 +1,28 @@
 // singleton
 // object literals
-const
+const JsUser={
+    name:"Divyanshi",
+    "full name":"Divynashi Khandelwal",
+    age:21,
+    email:"abc@gmail.com"
+}
+// console.log(JsUser["full name"]);
+JsUser.email="divi2@gamil.com"
+// Object.freeze(JsUser);
+JsUser.email="abc@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting=function()
+{
+console.log("Hello JS User");
+}
+
+JsUser.greetingTwo=function()
+{
+console.log(`Hello JS User ${this.name}`);
+}
+
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
