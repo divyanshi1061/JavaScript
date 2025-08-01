fe362d81d9864ccb73e8ebae999050328ed0ff9c
Divyanshi <109888817+divyanshi1061@users.noreply.github.com>
2025-08-01 09:11:03 +0000

    Functions

---
+++ basics2/functions.js
@@ -0,0 +1,17 @@
+function sayMyname(){
+    console.log("Hello");
+    
+}
+// sayMyname()
+
+function addTwonumbers(num1,num2){
+    console.log(num1+num2);
+    
+}
+// addTwonumbers(12,13)
+function loginUser(username)
+{
+   return `Welcome ,${username}`
+}
+console.log(loginUser("Divyanshi"));
+ 
