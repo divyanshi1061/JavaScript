748f4d8da4dd7cc17bfccc3b680393d125e4b3da
Divyanshi <109888817+divyanshi1061@users.noreply.github.com>
2025-08-01 03:32:29 +0000

    container config

--- basics2/array.js
+++ basics2/array.js
@@ -4,7 +4,24 @@ const arr= new Array(1,2,3,4)
 // console.log(arr[0]);
  
 arr.push(6)
-console.log(arr);
+// console.log(arr);
 arr.pop()
-console.log(arr)
+// console.log(arr)
+
+
+// ===================
+
+const marvel_heros=["throw","hulk","ironman","spiderman"]
+const dc_heros=["superman","batman","flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+
+// const combine=marvel_heros.concat(dc_heros)
+// console.log(combine);
+
+const all_combine=[...marvel_heros,...dc_heros];   // mostly used
+
+console.log(all_combine);
 
